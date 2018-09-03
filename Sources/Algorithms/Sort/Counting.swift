fd83811e2db0ebc060bfe59045b73be64c6935f0
Maxim Eremenko <devmeremenko@gmail.com>
2018-09-03 14:47:21 +0300

    Project: clean up

---
+++ Sources/Algorithms/Sort/Counting.swift
@@ -0,0 +1,53 @@
+//
+//  Counting.swift
+//  LeetCode
+//
+//  Created by Maxim Eremenko on 7/8/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class CountingSortExample: XCTestCase {
+    
+    func test() {
+        
+        let values = [3, -2, 1, 3, 1, -6, 1, -10, 3, 6, 2, 5, -6, 1, 0, 1, -4, 6, 10]
+        
+        XCTAssert(countingSort(values) == values.sorted())
+    }
+    
+    func countingSort(_ arr: [Int]) -> [Int] {
+        
+        /// Calculate a number of buckets in a range: min...max
+        let max = arr.max() ?? 0
+        let min = arr.min() ?? 0
+        let count = max - min + 1
+        
+        var countArray = [Int](repeating: 0, count: count)
+        for element in arr {
+            /// Increase a bucket that represents a respective number
+            countArray[element - min] += 1
+        }
+        
+        for index in 1 ..< countArray.count {
+            /// Set a number of values in the current and previous buckets.
+            /// It is useful when repeated items occur.
+            countArray[index] += countArray[index - 1]
+        }
+        
+        /// `Count array` now can help with mapping an item to the required position.
+        var sortedArray = [Int](repeating: 0, count: arr.count)
+        for item in arr {
+            /// Two reasons to decrease a value by 1
+            /// - any array's index starts from 0. values(indexes) in `count array`
+            ///   starts from 1.
+            /// - if there are duplicates, each of them should not be placed
+            ///   in the same position twice.
+            countArray[item - min] -= 1
+            sortedArray[countArray[item - min]] = item
+        }
+        
+        return sortedArray
+    }
+}
---
+++ Sources/Algorithms/Sort/Merge.swift
@@ -0,0 +1,65 @@
+//
+//  Merge.swift
+//  LeetCode
+//
+//  Created by Maxim Eremenko on 7/4/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class MergeSortExample: XCTestCase {
+    
+    func testMerging() {
+        
+        let first = [1, 3, 5, 6, 7, 8, 9, 9, 9, 9, 9]
+        let second = [2, 4, 6, 7, 8, 8, 8, 8, 9, 9]
+        
+        XCTAssert(merge(first, second) == (first + second).sorted())
+    }
+    
+    func testMergeSort() {
+        
+        let arr = [8, 3, 2, 6, 8, 2, 6, 1, 6, 2, 6, 1]
+        
+        XCTAssert(mergeSort(arr, 0, arr.count - 1) == arr.sorted())
+    }
+    
+    func mergeSort(_ arr: [Int], _ left: Int, _ right: Int) -> [Int] {
+        guard left < right else { return [arr[left]] }
+        
+        let mid = left + (right - left) / 2
+        let arr1 = mergeSort(arr, left, mid)
+        let arr2 = mergeSort(arr, mid + 1, right)
+        return merge(arr1, arr2)
+    }
+    
+    func merge(_ arr1: [Int], _ arr2: [Int]) -> [Int] {
+        var result = [Int]()
+        result.reserveCapacity(arr1.count + arr2.count)
+        
+        var i = 0
+        var j = 0
+        
+        while i < arr1.count && j < arr2.count {
+            
+            if arr1[i] < arr2[j] {
+                result.append(arr1[i])
+                i += 1
+            } else {
+                result.append(arr2[j])
+                j += 1
+            }
+        }
+        
+        if i == arr1.count {
+            result.append(contentsOf: arr2[j..<arr2.endIndex])
+        }
+        
+        if j == arr2.count {
+            result.append(contentsOf: arr1[i..<arr1.endIndex])
+        }
+        
+        return result
+    }
+}
