6dec6c4c65a58d624f0e8e845fef59e5c57eb689
Maxim Eremenko <devmeremenko@gmail.com>
2018-09-14 18:47:42 +0300

    CtCI: Delete Middle Node from a linked list (2.3) was solved

---
+++ Sources/Courses/CrackingTheCodingInterview/Chapter_2/DeleteMiddleNode_2_3.swift
@@ -0,0 +1,93 @@
+//
+//  DeleteMiddleNode_2_3.swift
+//  LeetCode
+//
+//  Created by Maxim Eremenko on 9/14/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class DeleteMiddleNodeTests: XCTestCase {
+    
+    /// Delete Middle Node
+    ///
+    /// Implement an algorithm to delete a node in the middle (i. e., any node
+    /// but the first and last node, not necessarily the exact middle) of a singly
+    /// linked list, given only access to that node.
+    ///
+    /// Note: You are not given access to the head of the linked list
+    ///
+    /// Input: the node c from the linked list a->b->c->d->e->f
+    /// Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+    
+    func test() {
+        
+        let data = createList()
+        
+        deleteMiddle(data.toDelete)
+        
+        verify(data.source, expected: data.expected)
+    }
+    
+    private func verify(_ source: Node, expected: Node) {
+        
+        /// make mutable
+        var sourceCheck: Node? = source
+        var expectedCheck: Node? = expected
+        
+        while sourceCheck != nil {
+            XCTAssert(sourceCheck?.value == expectedCheck?.value)
+            sourceCheck = sourceCheck?.next
+            expectedCheck = expectedCheck?.next
+        }
+    }
+}
+
+extension DeleteMiddleNodeTests {
+    
+    /// Time(1)
+    /// Space(1)
+    
+    private func deleteMiddle<Value: Equatable>(_ node: ListNode<Value>) {
+        
+        guard let next = node.next else {
+            return /// Note: the last node cannot be deleted. It may become a dummy node
+        }
+        
+        node.value = next.value
+        node.next = next.next
+    }
+}
+
+extension DeleteMiddleNodeTests {
+    
+    private typealias Node = ListNode<Int>
+    private typealias TestData = (source: Node, toDelete: Node, expected: Node)
+    
+    private func createList() -> TestData {
+        
+        let toDelete = ListNode(2)
+        
+        let source = ListNode(1)
+        source.next = toDelete
+        source.next?.next = ListNode(3)
+        source.next?.next?.next = ListNode(4)
+        
+        let expected = ListNode(1)
+        expected.next = ListNode(3)
+        expected.next?.next = ListNode(4)
+        
+        return (source, toDelete, expected)
+    }
+}
+
+private class ListNode<Value: Equatable> {
+    
+    var value: Value
+    var next: ListNode<Value>?
+    
+    init(_ value: Value) {
+        self.value = value
+    }
+}
