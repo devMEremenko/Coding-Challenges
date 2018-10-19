c3bc694a40eb7e6ac735c8162a5c453ad412b4c5
Maxim Eremenko <devmeremenko@gmail.com>
2018-10-19 20:34:15 +0300

    Adjacency List based on HashTable was implemented

---
+++ Sources/DataStructure/Graph/AdjacencyList+HashTable.swift
@@ -0,0 +1,84 @@
+//
+//  AdjacencyList+HashTable.swift
+//  LeetCode
+//
+//  Created by Maxim Eremenko on 10/27/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class AdjacencyListHashTableTests: XCTestCase {
+    
+}
+
+class HashGraph<Item: Hashable> {
+    
+    private var nodes = [Item: [Edge<Item>]]()
+}
+
+extension HashGraph: Graphable {
+    
+    func add(_ item: Item) {
+        
+        if nodes[item] == nil {
+            nodes[item] = []
+        }
+    }
+    
+    func addEdge(from: Item, to: Item, weight: Double?) {
+        
+        guard var edges = nodes[from] else { return }
+        
+        guard !edges.contains(where: { $0.to == to && $0.weight == weight }) else {
+            return
+        }
+        
+        edges.append(Edge(to, weight))
+        nodes[from] = edges
+    }
+    
+    func weight(from: Item, to: Item) -> Double? {
+        
+        guard let edges = nodes[from] else { return nil }
+        
+        return edges.first(where: { $0.to == to })?.weight
+    }
+    
+    var description: String {
+        
+        var result = ""
+        
+        for (item, edges) in nodes {
+            
+            result += "[ \(item) ->"
+            
+            for (idx, edge) in edges.enumerated() {
+                if idx == edges.count - 1 {
+                    result += " \(edge)"
+                } else {
+                    result += " \(edge), "
+                }
+            }
+            result += " ]\n"
+        }
+        
+        return result
+    }
+}
+
+private struct Edge<Item>: CustomStringConvertible {
+    
+    var to: Item
+    
+    var weight: Double?
+    
+    init(_ to: Item, _ weight: Double?) {
+        self.to = to
+        self.weight = weight
+    }
+    
+    var description: String {
+        return "\(to)" + (weight == nil ? "" : " \(weight!)")
+    }
+}
