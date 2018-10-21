741f4655e5da804e81fffd7e3934b9da667f878e
Maxim Eremenko <devmeremenko@gmail.com>
2018-10-21 20:36:00 +0300

    Route Between Nodes: in progress

---
+++ Sources/Courses/CrackingTheCodingInterview/Chapter_4 (Tree & Graph)/RouteBetweenNodes_4_1.swift
@@ -0,0 +1,19 @@
+//
+//  RouteBetweenNodes_4_1.swift
+//  LeetCode
+//
+//  Created by Maxim Eremenko on 10/22/18.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class RouteBetweenNodesTests: XCTestCase {
+    
+    /// Given a directed graph, design an algorithm to find out
+    /// whether there is a route between two nodes.
+    
+    func test() {
+        
+    }
+}
