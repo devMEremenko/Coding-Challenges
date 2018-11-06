55efa85f2011da87bad05efa472768cdd3dc2425
Maxim Eremenko <devMEremenko@gmail.com>
2018-11-06 15:56:26 +0200

    Optional: extension was added

---
+++ Sources/Extensions/Optional+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  Optional+Ext.swift
+//  LeetCode
+//
+//  Created by Maxim Eremenko on 31/12/2018.
+//  Copyright © 2018 Eremenko Maxim. All rights reserved.
+//
+
+import Foundation
+
+extension Optional {
+    
+    var notNil: Bool {
+        switch self {
+        case .none: return false
+        case .some: return true
+        }
+    }
+    
+    var isNil: Bool {
+        return !notNil
+    }
+}
