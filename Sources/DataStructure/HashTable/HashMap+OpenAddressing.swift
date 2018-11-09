b764d2f0558404794ef8ae45efd7330acec43eac
Maxim Eremenko <devMEremenko@gmail.com>
2018-11-09 15:59:20 +0200

    Comments clean up

--- Sources/DataStructure/HashTable/HashMap+OpenAddressing.swift
+++ Sources/DataStructure/HashTable/HashMap+OpenAddressing.swift
@@ -11,18 +11,3 @@ import XCTest
 /// TODO:
 /// HashTable based on the open addressing (with double hashing)
 ///
-
-
-
-/*
-@inline(__always) private func _index(for key: Key) -> Int {
-    return abs(key.hashValue) % buckets.count
-}
-
-@inline(__always) private func _offset(for key: Key, after index: Int) -> Int {
-    let constant = buckets.count - 1
-    let steps = constant - index % constant
-    let adjustedSteps = steps > 0 ? steps : 1
-    return (index + adjustedSteps) % buckets.count
-}
-*/
