75cdebdf7ca474c4b127b8f711405f79cb5ceb42
Maxim Eremenko <devMEremenko@gmail.com>
2019-06-22 14:45:15 +0300

    LeetCode: Backspace String Compare (844) was solved

---
+++ Sources/Courses/LeetCode/Easy/LE_BackspaceStringCompare_844.swift
@@ -0,0 +1,27 @@
+//
+//  LE_BackspaceStringCompare_844.swift
+//  CodingChallenges
+//
+//  Created by Maxim Eremenko on 05/07/2019.
+//  Copyright © 2019 Eremenko Maxim. All rights reserved.
+//
+
+import XCTest
+
+class LE_BackspaceStringCompare_844: XCTestCase {
+    
+    /// Given two strings S and T, return if they are equal
+    /// when both are typed into empty text editors. # means a backspace character.
+    /// https://leetcode.com/problems/backspace-string-compare/
+    
+    func test() {
+        
+    }
+}
+
+extension LE_BackspaceStringCompare_844 {
+    
+    func backspaceCompare(_ s: String, _ t: String) -> Bool {
+        
+    }
+}
