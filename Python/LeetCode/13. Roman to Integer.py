ad970d6e7509057eb0b06d71acd633a9071f94c6
Maxim Eremenko <devMEremenko@gmail.com>
2020-01-04 18:18:23 +0200

    Python, LeetCode, 13 upd

--- Python/LeetCode/13. Roman to Integer.py
+++ Python/LeetCode/13. Roman to Integer.py
@@ -1,13 +1,7 @@
 # https://leetcode.com/problems/roman-to-integer/
 
 class Solution:
-    # IV
-    # res = -1 + 5 = 4
         
-    # - create `result`
-    # - iterate from a first character to the end
-    # - inside a loop read a char and decide should a current value be subst from a result or added to the result. To do make a decicion, check rules.
-    
     def romanToInt(self, s: str) -> int:
         if not s: 
             return 0
