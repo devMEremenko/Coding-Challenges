0d74bc34aef65b690fbc07fbff1f8e914c988611
Maxim Eremenko <devmeremenko@gmail.com>
2021-03-06 20:17:36 +0200

    LeetCode: 669 solved (Added one more solution)

--- Python/LeetCode/669. Trim a Binary Search Tree.py
+++ Python/LeetCode/669. Trim a Binary Search Tree.py
@@ -7,14 +7,15 @@ class TreeNode:
         self.right = right
 
 class Solution:
-
     def trimBST(self, root, low, high):
-        return self.solution1(root, low, high)
+        # return self.solution1(root, low, high)
+        return self.solution2(root, low, high)
     
+    # Time O(n), Space O(height)
     def solution1(self, root, low, high):
         
         def solve(node):
-            if not node: return None
+            if not node: return node
             
             if node.val < low:
                 return solve(node.right)
@@ -27,3 +28,30 @@ class Solution:
         
         return solve(root)
     
+    # Time O(hight), Space O(1)
+    def solution2(self, root, low, high):
+        while True:
+            if not root:
+                return root
+            if root.val < low:
+                root = root.right
+            elif root.val > high:
+                root = root.left
+            else:
+                break
+        
+        parent = root
+        while parent.left:
+            if parent.left.val >= low:
+                parent = parent.left
+            else:
+                parent.left = parent.left.right
+                
+        parent = root
+        while parent.right:
+            if parent.right.val <= high:
+                parent = parent.right
+            else:
+                parent.right = parent.right.left
+        
+        return root
